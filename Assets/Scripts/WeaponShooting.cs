21a227931407f9f0b770d40914022da9bb3bf640
yobieesnezhinka <115245997+yobieesnezhinka@users.noreply.github.com>
2022-11-01 16:44:52 +0800

    RaycastShooting/Muzzles(Muzzle does not appear from Barrel)

--- Assets/Scripts/WeaponShooting.cs
+++ Assets/Scripts/WeaponShooting.cs
@@ -4,16 +4,55 @@ using UnityEngine;
 
 public class WeaponShooting : MonoBehaviour
 {
-    public Transform bulletSpawnPoint;
-    public GameObject bulletPrefab;
-    public float bulletSpeed = 50;
+    private float lastShootTime = 0;
+    
+    private Camera cam;
+    private Inventory inventory;
+    private EquipmentManager manager;
 
+    private void Start()
+    {
+        GetReferences();
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+            Shoot();
         }
+    }
+
+    private void RaycastShoot(Weapon currentWeapon)
+    {
+        Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        RaycastHit hit;
+
+        float currentWeaponRange = currentWeapon.range;
+
+        if (Physics.Raycast(ray, out hit,currentWeaponRange))
+        {
+            Debug.Log(hit.transform.name);
+        }
+
+        Instantiate(currentWeapon.muzzleFlashParticles, manager.currentWeaponBarrel);
+    }
+    private void Shoot()
+    {
+        Weapon currentWeapon = inventory.GetItem(manager.currentlyEquippedWeapon);
+
+        if(Time.time > lastShootTime + currentWeapon.fireRate)
+        {
+            Debug.Log("Shoot");
+            lastShootTime = Time.time;
+
+            RaycastShoot(currentWeapon);
+        }
+    }
+
+    private void GetReferences()
+    {
+        cam = GetComponentInChildren<Camera>();
+        inventory = GetComponent<Inventory>();
+        manager = GetComponent<EquipmentManager>();
     }
 }
