5a2bec77709fd8c45272d03d533d35e2f1253967
yobieesnezhinka <115245997+yobieesnezhinka@users.noreply.github.com>
2022-11-01 14:11:17 +0800

    Clunky Bullet and Shooting Mechanics (subjected to change)

--- Assets/Scripts/EquipmentManager.cs
+++ Assets/Scripts/EquipmentManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EquipmentManager : MonoBehaviour
-{
+{   
     private Animator anim;
     private Inventory inventory;
 
---
+++ Assets/Scripts/WeaponShooting.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponShooting : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 50;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+        }
+    }
+}
