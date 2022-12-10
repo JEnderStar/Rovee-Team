778aad062dce08e02e881e814248726a8a6a7662
yobieesnezhinka <115245997+yobieesnezhinka@users.noreply.github.com>
2022-12-10 21:43:10 +0800

    Imported Animation Rigging/Added head movement to NPC while Player is nearby

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -5,99 +5,29 @@ using UnityEngine.AI;
 
 public class FollowPlayer : MonoBehaviour
 {
-    NavMeshAgent agent;
+    public GameObject myTarget;
+    public NavMeshAgent myAgent;
+    private Animator anim;
 
-    Transform player;
+    public int range;
 
-    public LayerMask whatIsGround, whatIsPlayer;
-    private Animator anim = null;
-
-    //Patrol Variables
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPointRange;
-
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
-
-
-    void Awake()
+    private void Start()
     {
-        player = GameObject.Find("Player").transform;
-        agent = GetComponent<NavMeshAgent>();
         anim = GetComponentInChildren<Animator>();
     }
 
-    
-    void Update()
+    private void Update()
     {
-        //Check for sight and attack range
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        float dist = Vector3.Distance(this.transform.position, myTarget.transform.position);
 
-        if (!playerInSightRange && !playerInAttackRange)
+        if (dist <= myAgent.stoppingDistance)
         {
-            Patroling();
+            anim.SetFloat("Speed", 0f, 0.1f, Time.deltaTime);
         }
-
-        if (playerInSightRange && !playerInAttackRange)
+        else if(dist < range)
         {
-            StartWalking();
+            myAgent.destination = myTarget.transform.position;
+            anim.SetFloat("Speed", 1f, 0.1f, Time.deltaTime);
         }
-
-        if (playerInAttackRange && playerInSightRange)
-        {
-            StopWalking();
-        }
-    }
-
-    private void Patroling()
-    {
-        if (!walkPointSet)
-        {
-            SearchWalkPoint();
-        }
-
-        if (walkPointSet)
-        {
-            agent.SetDestination(walkPoint);
-            anim.SetFloat("Speed", 1f, 0.3f, Time.deltaTime);
-        }
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        //Walkpoint reached
-        if (distanceToWalkPoint.magnitude < 1f)
-        {
-            walkPointSet = false;
-        }
-    }
-
-    private void SearchWalkPoint()
-    {
-        //Calculate random point in range
-        float randomZ = Random.Range(-walkPointRange, walkPointRange);
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
-        {
-            walkPointSet = true;
-        }
-    }
-
-    private void StartWalking()
-    {
-        agent.SetDestination(player.position);
-        anim.SetFloat("Speed", 1f, 0.3f, Time.deltaTime);
-    }
-
-    private void StopWalking()
-    {
-        agent.SetDestination(transform.position);
-        anim.SetFloat("Speed", 0f);
-
-        transform.LookAt(player);
     }
 }
