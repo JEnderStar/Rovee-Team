fdf7137dba0da79b8adf70b455289911a0c404f8
JEnderStar <jayver_cuevas@yahoo.com>
2022-12-12 20:54:03 +0800

    added loading screen on Main Menu

---
+++ Assets/Scripts/LoadingScreenStartMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LoadingScreenStartMenu : MonoBehaviour
+{
+    public GameObject LoadingScreen;
+    public Slider LoadingBarFill;
+
+    public GameObject npc;
+    public TextMeshProUGUI text;
+
+    public void LoadScene()
+    {
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+
+        LoadingScreen.SetActive(true);
+        npc.SetActive(false);
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+            text.text = "Loading " + progressValue * 100f + "%";
+            LoadingBarFill.value = progressValue;
+            yield return null;
+        }
+    }
+}
