4a2a0dc583e92e2b08492bf2246a4bf3dde00bed
JEnderStar <jayver_cuevas@yahoo.com>
2022-12-18 04:06:20 +0800

    import tutorial images to tutorial scenes

--- Assets/Scripts/TouchToContinue.cs
+++ Assets/Scripts/TouchToContinue.cs
@@ -10,10 +10,17 @@ public class TouchToContinue : MonoBehaviour
     [SerializeField] TextMeshProUGUI text;
     [SerializeField] Slider LoadingBarFill;
 
+    [SerializeField] GameObject image;
+    [SerializeField] GameObject image1;
+    [SerializeField] GameObject image2;
+    [SerializeField] GameObject image3;
+    [SerializeField] GameObject image4;
+
     [SerializeField] GameObject textToEnable;
     [SerializeField] GameObject sliderToEnable;
     [SerializeField] GameObject textToDisable;
 
+    int touchesa = 1;
     void Update()
     {
         if (Input.touchCount > 0)
@@ -24,15 +31,64 @@ public class TouchToContinue : MonoBehaviour
             }
             if (Input.GetTouch(0).phase == TouchPhase.Ended)
             {
-                StartCoroutine(LoadSceneAsync());
+                if(touchesa == 1)
+                {
+                    image.SetActive(false);
+                    image1.SetActive(true);
+                    touchesa++;
+                }else if(touchesa == 2)
+                {
+                    image1.SetActive(false);
+                    image2.SetActive(true);
+                    touchesa++;
+                }else if(touchesa == 3)
+                {
+                    image2.SetActive(false);
+                    image3.SetActive(true);
+                    touchesa++;
+                }else if(touchesa == 4)
+                {
+                    image3.SetActive(false);
+                    image4.SetActive(true);
+                    touchesa++;
+                }else if(touchesa == 5)
+                {
+                    StartCoroutine(LoadSceneAsync());
+                }
             }
         }
         if (Input.GetMouseButtonDown(0))
         {
-            StartCoroutine(LoadSceneAsync());
+            if (touchesa == 1)
+            {
+                image.SetActive(false);
+                image1.SetActive(true);
+                touchesa++;
+            }
+            else if (touchesa == 2)
+            {
+                image1.SetActive(false);
+                image2.SetActive(true);
+                touchesa++;
+            }
+            else if (touchesa == 3)
+            {
+                image2.SetActive(false);
+                image3.SetActive(true);
+                touchesa++;
+            }
+            else if (touchesa == 4)
+            {
+                image3.SetActive(false);
+                image4.SetActive(true);
+                touchesa++;
+            }
+            else if (touchesa == 5)
+            {
+                StartCoroutine(LoadSceneAsync());
+            }
         }
     }
-
     IEnumerator LoadSceneAsync()
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
