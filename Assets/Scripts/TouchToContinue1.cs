517aaa83554ebdd7343e82cbd1366489e67136a5
JEnderStar <jayver_cuevas@yahoo.com>
2022-12-18 12:55:37 +0800

    fix some scenes

---
+++ Assets/Scripts/TouchToContinue1.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class TouchToContinue1 : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] Slider LoadingBarFill;
+
+    [SerializeField] GameObject textToEnable;
+    [SerializeField] GameObject sliderToEnable;
+    [SerializeField] GameObject textToDisable;
+
+    int touchesa = 1;
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                // nothing
+            }
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                    StartCoroutine(LoadSceneAsync());
+            }
+        }
+    }
+    IEnumerator LoadSceneAsync()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+
+        textToDisable.SetActive(false);
+        sliderToEnable.SetActive(true);
+        textToEnable.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+            text.text = "Loading " + progressValue * 100f + "%";
+            LoadingBarFill.value = progressValue;
+            yield return null;
+        }
+    }
+}
